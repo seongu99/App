000c316fbcf8ff16e824fad31b1c3f3e25e3bcff
seongu99 <seongu99@gmail.com>
2016-06-07 22:21:32 -0400

    1stWorkingOnTimerRun
    working on if statements.

--- app/src/main/java/com/example/seongu/myapplication/playActivity.java
+++ app/src/main/java/com/example/seongu/myapplication/playActivity.java
@@ -2,11 +2,21 @@ package com.example.seongu.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
 public class playActivity extends AppCompatActivity {
 
+    boolean finishFlag = true;
+    ResponseHandler handler = new ResponseHandler();
+
+    int remainingSets;
+    int remainingWorkMinTime;
+    int remainingWorkSecTime;
+    int remainingRestMinTime;
+    int remainingRestSecTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +38,73 @@ public class playActivity extends AppCompatActivity {
         txtRestMin.setText(String.valueOf(data.restMinTime));
         txtRestSec.setText(String.valueOf(data.restSecTime));
 
+        remainingSets = data.sets;
+        remainingWorkMinTime = data.workMinTime;
+        remainingWorkSecTime = data.workSecTime;
+        remainingRestMinTime = data.restMinTime;
+        remainingRestSecTime = data.restSecTime;
+
+        RequestThread thread = new RequestThread();
+        thread.start();
+
     }
+
+    class RequestThread extends Thread {
+        public void run(){
+            while(finishFlag){
+                timeDecrease();
+            }
+        }
+    }
+
+    private void timeDecrease() {
+        if(remainingSets == 0 && remainingWorkMinTime ==0 && remainingWorkSecTime ==0) {
+            finishFlag = false;
+
+        }
+        else if(remainingSets != 0 && remainingWorkMinTime != 0 && remainingWorkSecTime !=0){
+            try {
+                handler.sendEmptyMessage(remainingWorkSecTime--);
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+
+            }
+        }
+        else if(remainingSets != 0 && remainingWorkMinTime != 0 && remainingWorkSecTime ==0) {
+            try{
+
+                handler.sendEmptyMessage(remainingWorkMinTime--);
+                handler.sendEmptyMessage(remainingWorkSecTime = 59);
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+
+            }
+        }
+        else if (remainingSets != 0 && remainingWorkMinTime == 0 && remainingWorkSecTime ==0 && remainingWorkMinTime !=0 && remainingRestSecTime !=0) {
+            try {
+                handler.sendEmptyMessage(remainingRestSecTime--);
+                Thread.sleep(1000);
+
+            }catch (InterruptedException e) {
+
+            }
+        }
+        else if(remainingSets != 0 && remainingWorkMinTime == 0 && remainingWorkSecTime ==0 && remainingWorkMinTime !=0 && remainingRestSecTime ==0) {
+            try{
+                handler.sendEmptyMessage(remainingRestMinTime--);
+                handler.sendEmptyMessage(remainingRestSecTime = 59);
+                Thread.sleep(1000);
+            }catch(InterruptedException e) {
+
+            }
+        }
+    }
+
+    class ResponseHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+        }
+    }
+
 }
